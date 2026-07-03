e18e5701aec071cf3d2e5312ef572064a0080d0f
akshitravula <143444457+akshitravula@users.noreply.github.com>
2026-07-03 16:03:44 +0530

    Android app force download

--- src/components/layout/AndroidForcedInstall.jsx
+++ src/components/layout/AndroidForcedInstall.jsx
@@ -27,7 +27,7 @@ const AndroidForcedInstall = () => {
     <div style={{
       minHeight: '100dvh',
       width: '100vw',
-      backgroundImage: 'url("https://res.cloudinary.com/dcxlzfyfp/image/upload/v1783073741/img-to-link/xykx3k5eax8vjpw0axcx.webp")',
+      backgroundImage: 'url("https://res.cloudinary.com/dcxlzfyfp/image/upload/v1783074750/img-to-link/frihzx02hlozdlpwi2dq.webp")',
       backgroundSize: 'cover',
       backgroundPosition: 'top center',
       backgroundRepeat: 'no-repeat',
