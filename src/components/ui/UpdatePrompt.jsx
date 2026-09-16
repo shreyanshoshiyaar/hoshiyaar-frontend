e668452a96d6a04e6cb95d51b427313b98c2de72
akshitravula <143444457+akshitravula@users.noreply.github.com>
2026-09-16 19:56:22 +0530

    chore(release): bump android version code to 56 (v3.8.7)

--- src/components/ui/UpdatePrompt.jsx
+++ src/components/ui/UpdatePrompt.jsx
@@ -72,7 +72,7 @@ const UpdatePrompt = () => {
                 </a>
 
                 <p className="mt-4 text-xs text-gray-500 uppercase tracking-widest">
-                    v3.8.6 (Build {appVersion})
+                    v3.8.7 (Build {appVersion})
                 </p>
             </div>
         </div>
